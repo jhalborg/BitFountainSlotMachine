0817fe53dc25369a02685da2453cdefa51578237
jhalborg <jhalborg89@gmail.com>
2015-05-23 11:27:05 +0200

    Added assets

---
+++ Slot Machine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  Slot Machine
+//
+//  Created by Jesper Madsen on 23/05/15.
+//  Copyright (c) 2015 Jesper Madsen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
